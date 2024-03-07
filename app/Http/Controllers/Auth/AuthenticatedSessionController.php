70a3e521976573f2251f4ec989e884718f338a56
8320867686krish <154496866+8320867686krish@users.noreply.github.com>
2024-03-07 19:31:48 +0530

    changes for design

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -33,7 +33,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        Auth::logoutOtherDevices($request->input('password'));
+        //Auth::logoutOtherDevices($request->input('password'));
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
--- resources/views/client/clientAdd.blade.php
+++ resources/views/client/clientAdd.blade.php
@@ -24,7 +24,7 @@
         <!-- end pageheader -->
         <!-- ============================================================== -->
         <div class="row">
-            <div class="offset-xl-2 col-xl-8 col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
                 @include('layouts.message')
                 <div class="card">
                     <h5 class="card-header">Client Manager</h5>
