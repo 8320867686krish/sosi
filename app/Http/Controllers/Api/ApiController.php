f1f8cfe9d6de5cab040f12c08c6e544351cf1e95
8320867686krish <154496866+8320867686krish@users.noreply.github.com>
2024-03-29 18:14:55 +0530

    message

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -393,6 +393,7 @@ class ApiController extends Controller
             $decks = Deck::withCount('checks')->select('id', 'project_id', 'name','image','checks_count')->where('project_id', $project_id)->get();
             return response()->json(['isStatus' => true, 'message' => 'Project deck list retrieved successfully.', 'projectDeckList' => $decks,'mainPath' => url('images/projects/pdf/'.$project_id)]);
         } catch (Throwable $th) {
+            print_r($th->getMessage());
             return response()->json(['isStatus' => false, 'message' => 'An error occurred while processing your request.']);
         }
     }
