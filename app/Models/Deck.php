dbfa7d4a6fb921703d74042d8fa5173f90d0aeb7
8320867686krish <154496866+8320867686krish@users.noreply.github.com>
2024-03-29 18:17:45 +0530

    hidden filed

--- app/Models/Deck.php
+++ app/Models/Deck.php
@@ -10,6 +10,8 @@ class Deck extends Model
     use HasFactory;
 
     protected $fillable = ["project_id", "name", "image"];
+    protected $hidden = ['created_at', 'updated_at'];
+
     public function checks()
     {
         return $this->hasMany(Checks::class,'deck_id', 'id');
