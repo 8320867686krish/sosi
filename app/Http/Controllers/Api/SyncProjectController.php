d978fb65a1a08e94eb9bda2ab4cb612a76b3e29f
8320867686krish <154496866+8320867686krish@users.noreply.github.com>
2024-04-17 12:03:43 +0530

    revert back

--- app/Http/Controllers/Api/SyncProjectController.php
+++ app/Http/Controllers/Api/SyncProjectController.php
@@ -21,17 +21,14 @@ class SyncProjectController extends Controller
             return response()->json(['isStatus' => false, 'message' => 'Cant access.']);
 
         } else {
-            $projectList = collect();
-            Projects::with(['client:id,manager_name,manager_logo', 'decks.checks' => function ($query) {
-                $query->with(['check_image']);
-            }])->where('id', $projectId)->chunk(100, function ($projects) use (&$projectList) {
-                $projectList = $projectList->merge($projects);
-            }); // Adjust chunk size as needed
         
+                $project = Projects::with(['client:id,manager_name,manager_logo', 'decks.checks' => function ($query) {
+                    $query->with(['check_image']);
+                }])->find($projectId);
            
           
             
-            return response()->json(['isStatus' => true, 'message' => 'Project list retrieved successfully.', 'projectList' => $projectList]);
+            return response()->json(['isStatus' => true, 'message' => 'Project list retrieved successfully.', 'projectList' => $project]);
         }
       
     }
