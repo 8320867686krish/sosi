29bc46a4d678ac37d82789bab899ec7a92e20117
8320867686krish <154496866+8320867686krish@users.noreply.github.com>
2024-04-18 15:20:17 +0530

    zip create

--- app/Http/Controllers/Api/SyncProjectController.php
+++ app/Http/Controllers/Api/SyncProjectController.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use ZipArchive;
+use Illuminate\Support\Facades\File;
 
 class SyncProjectController extends Controller
 {
@@ -25,10 +27,30 @@ class SyncProjectController extends Controller
                 $project = Projects::with(['client:id,manager_name,manager_logo', 'decks.checks' => function ($query) {
                     $query->with(['check_image']);
                 }])->find($projectId);
-           
+                $sourceDir = public_path('images/pdf/'.$projectId);
+                $zipFilePath = public_path('images/pdf/'.$projectId.'.zip');
+                $zip = new ZipArchive;
+                if ($zip->open($zipFilePath, ZipArchive::CREATE) === TRUE) {
+                    $files = File::files($sourceDir);
+                
+                    foreach ($files as $key => $value) {
+                        $relativeNameInZipFile = basename($value);
+                        $zip->addFile($value, $relativeNameInZipFile);
+                    }
+                
+                    $zip->close();
+                
+                    // Check if the file exists before downloading
+                    if (!file_exists($zipFilePath)) {
+                        return response()->json(['isStatus'=>false,'error' => 'Failed to create zip file.']);
+                    }
+                } else {
+                    // Handle error if the zip archive cannot be opened or created
+                    return response()->json(['isStatus'=>false,'error' => 'Failed to create zip file.']);
+                }
           
             
-            return response()->json(['isStatus' => true, 'message' => 'Project list retrieved successfully.', 'projectList' => $project]);
+            return response()->json(['isStatus' => true, 'message' => 'Project list retrieved successfully.', 'projectList' => $project,'zipPath' => $zipFilePath]);
         }
       
     }
