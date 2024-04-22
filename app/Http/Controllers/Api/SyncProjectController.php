845dccc4dbe5034705336749d9fe22a061fea144
8320867686krish <154496866+8320867686krish@users.noreply.github.com>
2024-04-22 18:54:09 +0530

    print path

--- app/Http/Controllers/Api/SyncProjectController.php
+++ app/Http/Controllers/Api/SyncProjectController.php
@@ -57,7 +57,7 @@ class SyncProjectController extends Controller
                     // Fetch image data from storage or public directory
                     $imageFilename = basename($image->getOriginal('image'));
 
-                    $filePath = public_path('images/pdf/' . $projectId . '/' .$imageFilename); // Corrected file path
+                    $filePath = asset('images/pdf/' . $projectId . '/' .$imageFilename); // Corrected file path
                     echo  $filePath ;
                     // Make sure the file exists before adding it to the zip
                     if (file_exists($filePath)) {
