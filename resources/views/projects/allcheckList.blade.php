dcffc33948698796087eefe226c9a8bcb86f61d8
krishna <krishna.patel@meetanshi.com>
2024-05-02 18:55:47 +0530

    cheklist multiple row

---
+++ resources/views/projects/allcheckList.blade.php
@@ -0,0 +1,40 @@
+
+   
+            @if (isset($project->checks) && $project->checks->count() > 0)
+                @foreach ($project->checks as $check)
+                    @php $hazmatsCount = count($check->check_hazmats); @endphp
+                    @if($hazmatsCount == 0)
+                    <tr id="checkListTr_{{ $check->id }}">
+                   <td>{{ $check->initialsChekId }}</td>
+                   <td>{{ $check->name }}</td>
+                   <td>{{ $check->type }}</td>
+                   <td>{{ $check->location }}</td>
+                   <td>{{ $check->equipment }} <br> {{ $check->component }} </td>
+                   <td>&nbsp;</td>
+                   <td>&nbsp;</td>
+                   <td class="text-center">
+                           <a href="javascript:;" class="editCheckbtn" data-dotId="dot_{{ $loop->iteration }}" data-id="{{ $check->id }}"><i class="fas fa-edit text-primary" style="font-size: 1rem"></i></a>
+                           <a href="javascript:;" class="modalCheckbtn ml-2" data-id="{{ $check->id }}"><i class="fas fa-images text-primary" style="font-size: 1rem"></i></a>
+                   </td>
+               </tr>
+                    @endif
+                    @foreach ($check->check_hazmats as $index => $hazmat)
+                        <tr id="checkListTr_{{ $check->id }}">
+                            <td>{{ $check->initialsChekId }}</td>
+                            <td>{{ $check->name }}</td>
+                            <td>{{ $check->type }}</td>
+                            <td>{{ $check->location }}</td>
+                            <td>{{ $check->equipment }} <br> {{ $check->component }} </td>
+                          
+                            <td>{{ $hazmat->hazmat->name }}</td>
+                            <td>{{ $hazmat->type }}</td>
+                            <td class="text-center">
+                                    <a href="javascript:;" class="editCheckbtn" data-dotId="dot_{{ $loop->iteration }}" data-id="{{ $check->id }}"><i class="fas fa-edit text-primary" style="font-size: 1rem"></i></a>
+                                    <a href="javascript:;" class="modalCheckbtn ml-2" data-id="{{ $check->id }}"><i class="fas fa-images text-primary" style="font-size: 1rem"></i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                @endforeach
+              
+            @endif
+          
