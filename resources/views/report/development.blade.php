d8367f148e4bc5ba88eb18957ce241a9732d1756
krishna <krishna.patel@meetanshi.com>
2024-05-27 09:32:22 +0530

    pdf changes using mpdf

--- resources/views/report/development.blade.php
+++ resources/views/report/development.blade.php
@@ -1,25 +1,103 @@
+<!-- Section 1.1 -->
+<h2>3.Development of IHM of</h2>
+<div class="section-1-1">
+    <h3>3.1 Collection of Necessary Information</h3>
+    <ul>
+        <li>1 Ship's specification</li>
+        <li>2 Machinery Arrangement</li>
+    </ul>
 
-        <!-- Section 1.1 -->
-        <h2>3.Development of IHM of</h2>
-        <div class="section-1-1">
-            <h3>3.1	Collection of Necessary Information</h3>
-            <ul>
-                <li>1	Ship's specification</li>
-                <li>2	Machinery Arrangement</li>
-            </ul>
-  
-        </div>
-        <h3>3.2	 Indicative List</h3>
-        <h4>Materials to be listed from Table-A</h4>
-        <p>Materials to be listed from Table-A</p>
-        <ul>
-            <li>1. Asbestos </li>
-            <li>2. Polychlorinated biphenyls (PCBs) </li>
-            <li>3. Ozone-depleting substances  </li>
-            <li>4. Anti-fouling systems containing organotin compounds as a biocide or</li>
-            <li>5. Cybutryne</li>
-            <li>6. PFOS (as pe EUSRR & EMSA)</li>
-        </ul>
-        <h3>3.3	Working Procedure </h3>
-        <p>In order to create checks for the Visual Sampling Check Plan (VSCP) the following flow chart is referenced which is provided in MEPC 379(80) guidelines for preparation of the IHM.</p>
-        <img src="https://sosindi.com/IHM/public/assets/images/procedure.jpg">
+
+    <h3>3.2 Indicative List</h3>
+    <h4>Materials to be listed from Table-A</h4>
+    <p>Materials to be listed from Table-A</p>
+    <ul>
+        <li>1. Asbestos </li>
+        <li>2. Polychlorinated biphenyls (PCBs) </li>
+        <li>3. Ozone-depleting substances </li>
+        <li>4. Anti-fouling systems containing organotin compounds as a biocide or</li>
+        <li>5. Cybutryne</li>
+        <li>6. PFOS (as pe EUSRR & EMSA)</li>
+    </ul>
+    <h3>Materials listed from Table-B</h3>
+    <table>
+        <thead>
+            <tr>
+                <th>Materials</th>
+                <th>Component of equipment</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Cadmium and cadmium compounds</td>
+                <td>Plating film, bearing</td>
+            </tr>
+            <tr>
+                <td>Hexavalent chromium compounds</td>
+                <td>Plating film</td>
+            </tr>
+
+            <tr>
+                <td>Mercury and mercury compounds</td>
+                <td>Fluorescent light, mercury lamp, mercury cell, liquid-level switch, gyro compass, thermometer, measuring tool, manganese cell, pressure sensors,
+                    light fittings, electrical switches, fire detectors</td>
+            </tr>
+            <tr>
+                <td>Lead and lead compounds</td>
+                <td>Corrosion resistant primer, solder (almost all electric appliances contain solder), paints, preservative coatings, cable insulation, lead ballast, generators</td>
+            </tr>
+            <tr>
+                <td>Polybrominated biphenyls (PBBs)</td>
+                <td>Non-flammable plastics</td>
+            </tr>
+            <tr>
+                <td>Polybrominated diphenyl ethers (PBDE)</td>
+                <td>Non-flammable plastics</td>
+            </tr>
+            <tr>
+                <td>Polychlorinated naphthalenes</td>
+                <td>Paint, lubricating oil</td>
+            </tr>
+            <tr>
+                <td>Radioactive substances</td>
+                <td>    <b>consumer products with radioactive materials</b>
+                <ul style="list-style-type:circle">
+                    <li> Ionization chamber smoke detectors (typical radionuclides 241Am; 226Ra)</li>
+                    <li> Instruments/signs containing gaseous tritium light sources (3H)</li>
+                    <li>Instruments/signs containing radioactive painting (typical radionuclide 226Ra)</li>
+                    <li> High intensity discharge lamps (typical radionuclides 85Kr; 232Th)</li>
+                    <li>Radioactive lighting rods (typical radionuclides 241Am; 226Ra)</li>
+                </ul>
+
+                    <b>industrial gauges with radioactive materials</b>
+                    <ul>
+                    <li>Radioactive level gauges</li>
+                    <li>Radioactive dredger gauges</li>
+                    <li>Radioactive conveyor gauges</li>
+                    <li>Radioactive spinning pipe gauges</li>
+                </ul>
+                </td>
+            </tr>
+        </tbody>
+
+    </table>
+    <h3>3.3 Working Procedure </h3>
+    <p>In order to create checks for the Visual Sampling Check Plan (VSCP) the following flow chart is referenced which is provided in MEPC 379(80) guidelines for preparation of the IHM.</p>
+
+
+    <img src="https://sosindi.com/IHM/public/assets/images/procedure.jpg">
+    <h4 style="font-weight: bold;text-align:center;padding-top:10px">Figure-1 Flowchart for IHM Checks</h4>
+    <div style="text-align:justify;padding:10px">
+        <p>1. Documents may include certificates, manuals, ship’s plans drawings, technical specifications and information from sister and/or similar ships.</p>
+
+        <p>2. The assessment should cover all materials listed in table A of Appendix 1 of the Guideline; the control areas listed in table B should be assessed as practicable.<br />It is assumed not to be able to contain hazardous materials described above for areas assessed not equivalent listed in table 5 should be used as far as available.<br />Where possible additional documents related to knowledge gained during previous assessments may be used based on knowledge and logic.</p>
+
+        <p>3. Equipment must be made clear which cannot and/or are area specific. It must be system and/or areas which cannot be specified shall be included in the record of the assessment.In addition, system scope of this assessment can become continuous sampling listed in Appendix 1 of these guidelines on a basis of the results just taken. In the conclusion, this document system incorporates documentation containing List required equipment/materials necessary to perform testing on the equipment. Each piece of equipment is an operational state efficiency at peak working hours each day before it is impossible to assess all equipment without compromising ship safety without operational efficiency.</p>
+
+        <p>4. When assessing pieces that incorporate sampling identification badges/documents can also take into account age and tech state by comparing materials used in Appendix 1 List for classification systems with those currently present. In the prerequisite for this assessment method are described how best testing techniques are made more comprehensive by checking history records required hazardous material’s disposal documentation contained in appendix systems should also consider current “as is” position or any changes that have been made since last checked.</p>
+
+        <p>5. When equipment, systems and/or area of ship are not accessible for visual check or sampling checks, this equipment system and/or area is classified as ‘Potentially Containing Hazardous Materials (PCHM)’</p>
+    </div>
+
+
+</div>
