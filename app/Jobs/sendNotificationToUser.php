92af1582a818f99669fccf597d67e8b081efe0f3
krishna <krishna.patel@meetanshi.com>
2024-07-10 18:47:36 +0530

    set mail

--- app/Jobs/sendNotificationToUser.php
+++ app/Jobs/sendNotificationToUser.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-
+use Mail;
 class sendNotificationToUser implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
