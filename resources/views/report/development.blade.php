e98c29335e0ec223d3772d77d2e07646c7ce72b0
krishna <krishna.patel@meetanshi.com>
2024-08-23 19:18:43 +0530

    resolve error

--- resources/views/report/development.blade.php
+++ resources/views/report/development.blade.php
@@ -171,7 +171,6 @@
 
 
                     @endphp
-                    @if(count($ex_boiler) > 1)
                     <tr>
                         <td rowspan="{{count($ex_boiler)}}">Boiler<br />
                             @foreach($json_boiler as $value)
@@ -335,7 +334,7 @@
                                         $ex_boiler = @$foundItems['Air conditioning system']['component'] ?? [];
                                         $json_boiler = @$foundItems['Air conditioning system']['make'] ?? [];
                                         @endphp
-                                        @if(count($ex_boiler)>1)
+                                        
                                         <tr>
                                             <td rowspan="{{count($ex_boiler)}}">Air conditioning system<br />
                                                 @foreach($json_boiler as $value)
@@ -350,7 +349,6 @@
                                                 @endforeach
                                             <td>{{@$ex_boiler[0]}}</td>
                                         </tr>
-                                        @endif
                                         @if(@$ex_boiler)
                                         @for($i = 1; $i < count($ex_boiler); $i++) <tr>
                                             <td>{{ $ex_boiler[$i] }}</td>
