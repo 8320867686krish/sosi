adb824ee7da95212d6e3e040bcabf45e4322d6d3
krishna <krishna.patel@meetanshi.com>
2024-09-02 13:54:09 +0530

    summery report

--- resources/views/report/cover.blade.php
+++ resources/views/report/cover.blade.php
@@ -1,6 +1,9 @@
 <div>
     <div style="text-align: center;">
-    <h1 style="padding-top: 10px;">{{$projectDetail['ihm_table']}} {{@$summery ?? $summery : ''}}Report</h1>
+    <h1 style="padding-top: 10px;">
+    {{$projectDetail['ihm_table']}} {{ $summery ?? '' }} Report
+</h1>
+
 
 
     <div>
