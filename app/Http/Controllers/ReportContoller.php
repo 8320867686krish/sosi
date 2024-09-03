73980682929e1b3014b0b78fe21eaae3e0eb40d4
krishna <krishna.patel@meetanshi.com>
2024-09-03 18:24:58 +0530

    value print

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -800,16 +800,17 @@ class ReportContoller extends Controller
     public function demoTest(){
         $ChecksList = CheckHasHazmat::get();
         foreach($ChecksList as $value){
-            $check_id = $value['check_id'];
-            $hazmat_id = $value['hazmat_id '];
-            $lebResurt = LabResult::where('check_id',$check_id)->where('hazmat_id',$hazmat_id)->first();
-            print_r( $lebResurt);
-            if($lebResurt){
-                $type = $lebResurt['type'];
-            }else{
-                $type = 'Not Contained';
-            }
-           CheckHasHazmat::where('id',$value['id'])->update(['final_lab_result'=>$type]);
+            print_r($value);
+        //     $check_id = $value['check_id'];
+        //     $hazmat_id = $value['hazmat_id '];
+        //     $lebResurt = LabResult::where('check_id',$check_id)->where('hazmat_id',$hazmat_id)->first();
+        //     print_r( $lebResurt);
+        //     if($lebResurt){
+        //         $type = $lebResurt['type'];
+        //     }else{
+        //         $type = 'Not Contained';
+        //     }
+        //    CheckHasHazmat::where('id',$value['id'])->update(['final_lab_result'=>$type]);
         }
     }
 }
