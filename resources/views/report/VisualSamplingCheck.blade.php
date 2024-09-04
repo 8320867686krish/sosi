51cf35938693cc73c98425b6e7c79d9808b3fe06
krishna <krishna.patel@meetanshi.com>
2024-09-04 19:51:48 +0530

    changes during check add table in report

--- resources/views/report/VisualSamplingCheck.blade.php
+++ resources/views/report/VisualSamplingCheck.blade.php
@@ -8,9 +8,9 @@
         <table>
             <thead>
                 <tr>
-                <th colspan="2">Result of Documentation Analysis</th>
-                <th colspan="2">Procedure of Check</th>
-                <th colspan="2">Result of Check</th>
+                    <th colspan="2">Result of Documentation Analysis</th>
+                    <th colspan="2">Procedure of Check</th>
+                    <th colspan="2">Result of Check</th>
                 </tr>
             </thead>
             <tbody>
@@ -31,24 +31,24 @@
                     <td rowspan="2">Contained</td>
                 </tr>
                 <tr>
-                    
+
                     <td>Unknown</td>
                     <td>Unable to conclude</td>
                 </tr>
                 <tr>
                     <td rowspan="2">Assumption</td>
                     <td rowspan="2">Assumptions based on referred guidelines/reports</td>
-                 
-                    <td  rowspan="2">PCHM</td>
-                    <td  rowspan="2">Potentially Contained Hazardous material</td>
+
+                    <td rowspan="2">PCHM</td>
+                    <td rowspan="2">Potentially Contained Hazardous material</td>
                     <td>BTV</td>
                     <td>Below Threshold Value</td>
                 </tr>
-              
+
             </tbody>
 
         </table>
-        <br/>
+        <br />
         <table>
             <thead>
                 <tr>
@@ -74,7 +74,7 @@
                 @php $hazmatsCount = count($check->check_hazmats); @endphp
                 @if ($hazmatsCount == 0)
                 <tr>
-                <td>{{$check['name']}}</td>
+                    <td>{{$check['name']}}</td>
                     <td>&nbsp;</td>
                     <td>&nbsp;</td>
                     <td>{{ $check->location }} @if($check->sub_location){{ ',' . $check->sub_location }}
@@ -93,23 +93,23 @@
                 $color = 'style="color:#FF0000"';
 
                 }else{
-                    $color = 'style="color:#000000"';
+                $color = 'style="color:#000000"';
                 }
                 @endphp
                 <tr>
 
-                <td  {!! $color !!}>{{$check['name']}}</td>
+                    <td {!! $color !!}>{{$check['name']}}</td>
 
-             <td  {!! $color !!}>{{ $hazmat->hazmat->table_type}}</td>
-                    <td  {!! $color !!}>{{ $hazmat->hazmat->short_name }}</td>
+                    <td {!! $color !!}>{{ $hazmat->hazmat->table_type}}</td>
+                    <td {!! $color !!}>{{ $hazmat->hazmat->short_name }}</td>
                     <td {!! $color !!}>{{ $check->location }} @if($check->sub_location){{ ',' . $check->sub_location }}
                         @endif</td>
                     <td {!! $color !!}>{{ $check->equipment }}</td>
-                    <td  {!! $color !!}> {{ $check->component }}</td>
+                    <td {!! $color !!}> {{ $check->component }}</td>
                     @if($hazmat->image)
-                    <td  {!! $color !!}><a href="{{$hazmat->image}}" target="_blank">Known</a></td>
+                    <td {!! $color !!}><a href="{{$hazmat->image}}" target="_blank">Known</a></td>
                     @else
-                    <td  {!! $color !!}>UnKnown</td>
+                    <td {!! $color !!}>UnKnown</td>
                     @endif
 
                     <td {!! $color !!}>{{ ($check->type == 'visual') ? 'V' : 'S' }}</td>
@@ -124,6 +124,7 @@
             </tbody>
         </table>
     </div>
+    <div  class="section-1-1">
     <h2>Record of Changes in Checks During Onboard Survey</h2>
     <table>
         <thead>
@@ -133,24 +134,26 @@
                 <th>Changed To</th>
                 <th>Remark</th>
             </tr>
-            <tbody>
-                @foreach($ChangeCheckList as $value)
-                <tr>
-                    <td>{{$value['name']}}</td>
-                    <td>{{$value['type']}}</td>
-                    <td>
+        <tbody>
+            @foreach($ChangeCheckList as $value)
+            <tr>
+                <td>{{$value['name']}}</td>
+
+                <td>
                     @if($value->markAsChange == 1)
                     {{ $value->type == 'smaple' ? 'visual' : 'sample' }}
                     @else
                     {{ $value->type }}
                     @endif
                 </td>
+                <td>{{$value['type']}}</td>
                 <td>
-                    {{$value->labResultsChange}}
+                    {{$value->labResultsChange->lab_remarks}}
                 </td>
-                </tr>
-                @endforeach
-            </tbody>
+            </tr>
+            @endforeach
+        </tbody>
         </thead>
     </table>
+    </div>
 </div>
