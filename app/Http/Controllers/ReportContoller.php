161aea8e933e6f8512d0eba1d02247ef233a9179
krishna <krishna.patel@meetanshi.com>
2024-09-06 18:29:39 +0530

    set end of report

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -574,11 +574,7 @@ class ReportContoller extends Controller
             }
         }
         
-        $mpdf->WriteHTML('
-        <div  style="text-align:center;position:absolute; bottom:100px; width:100%;left:30%">
-            <b>...End Of The IHM Report...</b>
-        </div>
-    ');
+      
     
         $safeProjectNo = str_replace('/', '_', $projectDetail['project_no']);
 
