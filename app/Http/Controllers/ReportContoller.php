e8d810d3daa522884134cffe1ab74e5a990f7ff1
krishna <krishna.patel@meetanshi.com>
2024-09-11 14:23:42 +0530

    now resolved

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -748,11 +748,10 @@ class ReportContoller extends Controller
                         $tooltipStart = $image_height + $gap;
                         $sameLocation = 0;
                         foreach ($evenarrayLeft as $key => $evenvalue) {
-                            if (abs($lineLeftPosition - $evenvalue) < 100 && abs($topshow - $evenarrayTop[$key]) < 100) {
+                            if (abs($lineLeftPosition - $evenvalue) < 100) {
                                 $sameLocation++;
                                 $tooltipStart = $tooltipStart +  $evenincreaseGap;
                                 $lineHeight = $lineHeight +  $evenincreaseGap;
-                                $tooltipText ="Pr";
                             }else{
                                 $tooltipText = abs($topshow - $evenarrayTop[$key]);
                             }
