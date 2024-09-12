f14c0c4f0c8890eaea8fa3c6bf3a004e711e63b3
krishna <krishna.patel@meetanshi.com>
2024-09-12 15:12:00 +0530

    same height

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -759,11 +759,9 @@ class ReportContoller extends Controller
                           
                             else{
                                 $sameLocation++;    
-                                if( abs($lineHeight - $evenarrayLineHeight[$key]) > 100){
-                                    $tooltipStart = $tooltipStart - 15;
-                                    $lineHeight = $lineHeight -15; 
-                                    $tooltipText = "e".$k.":".$tooltipStart;
-                                }
+                                $tooltipStart = $tooltipStart + $evenincreaseGap ;
+                                $lineHeight = $lineHeight + $evenincreaseGap;
+                              
                                
 
                              }
