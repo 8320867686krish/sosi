fd31877a70bd318165c03cceb71372ed3fe953cf
krishna <krishna.patel@meetanshi.com>
2024-09-12 12:16:16 +0530

    some value plus

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -766,7 +766,7 @@ class ReportContoller extends Controller
                             else{
                                 $tooltipText = "e".$k;
                                 if($k>12){
-                                    $kvalue =  $k + 3;
+                                    $kvalue =  $k + 2;
                                 }else{
                                     $kvalue = 0;
                                 }
