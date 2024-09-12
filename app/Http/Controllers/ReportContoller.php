75f9a6492211dcebbf2b8fb8657d8979c8019c81
krishna <krishna.patel@meetanshi.com>
2024-09-12 15:53:18 +0530

    now decrement

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -751,8 +751,8 @@ class ReportContoller extends Controller
                                 $sameLocation++;
                                 $tooltipText = $lineHeight . ";" . $k;
                                 if (abs($lineHeight - $evenarrayLineHeight[$key] > 100)) {
-                                    $tooltipStart = $tooltipStart + 20;
-                                    $lineHeight = $lineHeight + 20;
+                                    $tooltipStart = $tooltipStart + 15;
+                                    $lineHeight = $lineHeight + 15;
                                 } else {
                                     $tooltipStart = $tooltipStart + $evenincreaseGap;
                                     $lineHeight = $lineHeight + $evenincreaseGap;
