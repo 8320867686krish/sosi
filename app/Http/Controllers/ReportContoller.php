cd6739538583e7a83fc50be1e2e489ded6ebd3de
krishna <krishna.patel@meetanshi.com>
2024-09-12 19:51:30 +0530

    set 80

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -755,8 +755,8 @@ class ReportContoller extends Controller
                                 //     $lineHeight = $lineHeight + 15;
                                 // } else {
                                 if (abs($tooltipStart  - 210) < 50) {
-                                    $tooltipStart = $tooltipStart - 100;
-                                    $lineHeight = $lineHeight -  100;
+                                    $tooltipStart = $tooltipStart - 80;
+                                    $lineHeight = $lineHeight -  80;
                                    
                                 } else {
                                     $tooltipStart = $tooltipStart + $evenincreaseGap;
