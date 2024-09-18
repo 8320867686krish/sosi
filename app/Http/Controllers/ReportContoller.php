08fb5d1cd746cc9e0270c91eeec80315ec10d7b3
krishna <krishna.patel@meetanshi.com>
2024-09-18 19:41:35 +0530

    set proper

--- app/Http/Controllers/ReportContoller.php
+++ app/Http/Controllers/ReportContoller.php
@@ -645,7 +645,7 @@ class ReportContoller extends Controller
                     if ($height >= 380) {
                         $ori = "portrait";
                         if($width >= 500){
-                            $containerWidth = "0";
+                            $containerWidth = "500";
 
                         }else{
                             $containerWidth = "900";
